462f2848c209878136c8e12105232e2efc8b295d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-28 14:14:48 +0530

    html structure added

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -4,7 +4,12 @@ import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
 const TicTacToe = () => {
   return (
-    <div>
+    <div className='container'>
+        <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
+        <div className="board">
+
+        </div>
+        <button className="reset">Reset</button>
     </div>
   )
 }
