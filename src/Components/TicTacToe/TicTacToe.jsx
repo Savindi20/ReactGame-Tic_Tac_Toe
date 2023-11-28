b752f5a861375be94c271c922dbe47ce47e209bd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-28 19:35:07 +0530

    logic basic added

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./TicTacToe.css'
 import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
+
+let data = ["","","","","","","","",""];
+
 const TicTacToe = () => {
+
+    let [count,setCount] = useState(0);
+    let [lock,setLock] = useState(false);
+
   return (
     <div className='container'>
         <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
