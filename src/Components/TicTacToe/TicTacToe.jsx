e23a61d66b6b9119f65048f2d90ae6b557c19cda
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-28 20:00:01 +0530

    toggle logic part added

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -10,6 +10,22 @@ const TicTacToe = () => {
     let [count,setCount] = useState(0);
     let [lock,setLock] = useState(false);
 
+    const toggle = (e,num) => {
+        if(lock){
+            return 0;
+        }
+        if(count%2===0){
+            e.target.innerHTML = `<img src ='${cross_icon}'>`;
+            data[num]="x";
+            setCount(++count);
+        }
+        else{
+            e.target.innerHTML = `<img src ='${circle_icon}'>`;
+            data[num]="o";
+            setCount(++count);
+        }
+    }
+
   return (
     <div className='container'>
         <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
